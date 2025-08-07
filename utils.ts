752089d932031490de53a490da7f954d8b6f5cde
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-08-08 00:28:41 +0900

    Add rudimentary caching

--- utils.ts
+++ utils.ts
@@ -1,3 +1,9 @@
+import { tmpdir } from "node:os";
+import * as path from "node:path";
+import { mkdirSync, writeFileSync, readFileSync, existsSync } from "node:fs";
+import { hash } from "node:crypto";
+import { cwd } from "node:process";
+
 import * as z from "npm:zod";
 import { $ } from "npm:zx";
 import { XMLParser } from "npm:fast-xml-parser";
@@ -23,10 +29,23 @@ export async function fetchLogEntries(opts?: {
   limit?: string;
   revision?: string;
 }) {
-  const output = await $`svn log --xml ${[
-    ...(opts?.limit ? ["--limit", opts.limit] : []),
-    ...(opts?.revision ? ["--revision", opts.revision] : []),
-  ]}`.text();
+  // We want it to persist.
+  const cacheFile = path.join(
+    tmpdir(),
+    `k-gitsvn-${hash("md5", JSON.stringify({ dir: cwd(), ...opts }))}`
+  );
+  const cacheHit = existsSync(cacheFile);
+  const output = cacheHit
+    ? readFileSync(cacheFile, { encoding: "utf-8" })
+    : await $`svn log --xml ${[
+        ...(opts?.limit ? ["--limit", opts.limit] : []),
+        ...(opts?.revision ? ["--revision", opts.revision] : []),
+      ]}`.text();
+  if (!cacheHit) {
+    mkdirSync(path.dirname(cacheFile), { recursive: true });
+    writeFileSync(cacheFile, output, { encoding: "utf-8" });
+  }
+
   const parser = new XMLParser({ ignoreAttributes: false });
   const raw = parser.parse(output);
   return SvnLogRaw.parse(raw).log.logentry;
